e388c893619234769deea82789648c1ef1edf912
westronham <westronham@users.noreply.github.com>
2017-07-11 07:36:17 +1000

    Update enumero.sh

--- enumero.sh
+++ enumero.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
 usage() { 
-    #echo "Usage: $0 -i <IP> -d <Work Directory> -e <Interface>]" 1>&2; exit 1; 
-cat<<EOF
+    cat<<EOF
     Usage: $0 <Options>
     
     This script does stuff...
     
     OPTIONS:
-        -h              Get help
-        -i <IP>         IP Address
-        -d <Directory>  Work Directory
-        -e <Interface>  Interface
+        -h..................Get help
+        -i <IP>.............IP Address
+        -d <Directory>......Work Directory
+        -e <Interface>......Interface
 EOF
+    exit 1
 }
 
 while getopts ":i:d:e:" option
@@ -27,12 +31,13 @@ do
             ;;
         d)
             WORK_DIRECTORY=${OPTARG}
+            mkdir -p ${WORK_DIRECTORY}/raw
             ;;
         e)
             INTERFACE=${OPTARG}
             ;;
         ?) 
-            echo -e "   ERROR: Invalid option: -$OPTARG\n" >&2
+            echo -e "${RED}    ERROR: Invalid option: -${OPTARG}\n${NC}" >&2
             usage
             exit
             ;;
@@ -40,18 +45,33 @@ do
 done
 shift "$((OPTIND-1))"
 
-if [[ -z $IP_ADDRESS ]] || [[ -z $WORK_DIRECTORY ]] || [[ -z $INTERFACE ]]
+# Check that required flags are there
+if [[ -z ${IP_ADDRESS} ]] || [[ -z ${WORK_DIRECTORY} ]] || [[ -z ${INTERFACE} ]]
 then
     usage
     exit 1
 fi
 
-# Change colour
-echo "nmap -n -sn $IP_ADDRESS -oA $WORK_DIRECTORY/raw/nmap_ping_sweep"
-# Need to do escaping on the IP_ADDRESS
-nmap -n -sn $IP_ADDRESS -oA "$WORK_DIRECTORY/raw/nmap_ping_sweep"
+# Check for root (some tools require root for certain activities):
+if [ "$(id -u)" != "0" ]; then
+    echo -e "${RED}    ERROR: This script must be run as root${NC}"
+    exit 1
+fi
 
-# Change colour
-echo "unicornscan -i $INTERFACE -Iv -mT -r7500 $IP_ADDRESS:0-65535 | tee $WORK_DIRECTORY/unicornscan_port_sweep"
-# Need to do escaping on IP_ADDRESS
-unicornscan -i $INTERFACE -Iv -mT -r7500 $IP_ADDRESS:0-65535 | tee $WORK_DIRECTORY/unicornscan_port_sweep
+echo -e "${BLUE}\n*** nmap -n -sn ${IP_ADDRESS} -oA ${WORK_DIRECTORY}/raw/nmap_ping_sweep__${IP_ADDRESS/\//_} ***\n${NC}"
+nmap -n -sn ${IP_ADDRESS} -oA ${WORK_DIRECTORY}/raw/nmap_ping_sweep__${IP_ADDRESS/\//_}
+
+echo -e "${BLUE}\n*** unicornscan -i ${INTERFACE} -Iv -mT -r7500 ${IP_ADDRESS}:0-65535 | tee ${WORK_DIRECTORY}/raw/unicornscan_tcp_port_sweep__${IP_ADDRESS/\//_} ***\n${NC}"
+# For a list of IP addresses, need to do unicornscan [IP]:0-65535 [IP]:0-65535 etc.
+unicornscan -i ${INTERFACE} -Iv -mT -r7500 ${IP_ADDRESS}:0-65535 | tee ${WORK_DIRECTORY}/raw/unicornscan_tcp_port_sweep__${IP_ADDRESS/\//_}
+
+awk '/^scaning/{flag=1;next}/^sender/{flag=0}flag' ${WORK_DIRECTORY}/raw/unicornscan_port_sweep__${IP_ADDRESS/\//_} | awk '/.+/ {print $3}' | sort -t: -nk2 | gawk -F: '
+    /.+/{
+            if (!($1 in arr)) { key[++i] = $1; }
+            arr[$1] = arr[$1] $2 ",";
+        }
+    END {
+        for (j = 1; j <= i; j++) {
+            printf("%s:%s%s",  key[j], arr[key[j]], (j == i) ? "" : "\n");
+        }
+    }' | sed 's/,$//' | sort -t: -nk1 > ${WORK_DIRECTORY}/discovered_hosts.txt
